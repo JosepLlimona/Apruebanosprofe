15ea78008d9b11d52f105661491640c9853b2e3c
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-16 15:15:32 +0200

    bugs fixed

--- RPG_GAME/Scripts/Main.gd
+++ RPG_GAME/Scripts/Main.gd
@@ -9,6 +9,7 @@ var ESC_PERS:= preload("res://Scenes/Player.tscn")
 var first_over=true
 # Called when the node enters the scene tree for the first time.
 
+var currentSpace = null
 
 func _ready():
 	first_over=true
@@ -33,74 +34,89 @@ func iniciar_ciutat():
 	$".".move_child($Player, 2)
 	$Player.set_scale(Vector2(5,5))
 	$Player.set_global_position($TileMap/Position2D.get_global_position())
+	currentSpace = ciutat
 
 func iniciar_dungeon_1():
 	Global.pos_over=$Player.get_global_position()
-	$".".remove_child($TileMap)
+	get_child(0).queue_free()
 	var dung1 = ESC_DUNG_1.instance() 
-	$".".add_child(dung1, true)	
-	$Player.set_global_position($TileMap/Position2D.get_global_position())
+	call_deferred("add_child", dung1)
+	call_deferred("move_child", dung1,0)
+	$Player.set_global_position(dung1.get_node("Position2D").position)
 	#camera limits
-	var map_limits = $TileMap.get_used_rect()
-	var map_cellsize = $TileMap.cell_size
+	var map_limits = dung1.get_used_rect()
+	var map_cellsize = dung1.cell_size
 	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
 	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
 	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-	$".".move_child($Player, 2)
-	for child in $TileMap.get_children():
+	Global.enemies.clear()
+	for child in dung1.get_children():
 		if child.is_class("Path2D"):
 			Global.enemies.append(child)
+	currentSpace = dung1
 
 func iniciar_dungeon_2():
 	Global.pos_over=$Player.get_global_position()
-	$".".remove_child($TileMap)
+	get_child(0).queue_free()
 	var dung2 = ESC_DUNG_2.instance() 
-	$".".add_child(dung2, true)	
-	$Player.set_global_position($TileMap/Position2D.get_global_position())
+	call_deferred("add_child", dung2)
+	call_deferred("move_child", dung2,0)
+	$Player.set_global_position(dung2.position)
 	#camera limits
-	var map_limits = $TileMap.get_used_rect()
-	var map_cellsize = $TileMap.cell_size
+	var map_limits = dung2.get_used_rect()
+	var map_cellsize = dung2.cell_size
 	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
 	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
 	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-	$".".move_child($Player, 2)
+	Global.enemies.clear()
+	for child in dung2.get_children():
+		if child.is_class("Path2D"):
+			Global.enemies.append(child)
+	currentSpace = dung2
 
 func iniciar_dungeon_3():
 	Global.pos_over=$Player.get_global_position()
-	$".".remove_child($TileMap)
+	get_child(0).queue_free()
 	var dung3 = ESC_DUNG_3.instance() 
-	$".".add_child(dung3, true)	
-	$Player.set_global_position($TileMap/Position2D.get_global_position())
+	call_deferred("add_child", dung3)
+	call_deferred("move_child", dung3, 0)
+	$Player.set_global_position(dung3.get_node("Position2D").position)
 	#camera limits
-	var map_limits = $TileMap.get_used_rect()
-	var map_cellsize = $TileMap.cell_size
+	var map_limits = dung3.get_used_rect()
+	var map_cellsize = dung3.cell_size
 	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
 	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
 	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-	$".".move_child($Player, 2)
+	Global.enemies.clear()
+	for child in dung3.get_children():
+		if child.is_class("Path2D"):
+			Global.enemies.append(child)
+	currentSpace = dung3
 
 func iniciar_overworld():
-	$".".remove_child($TileMap)
+	$TileMap.queue_free()
 	var overworld = ESC_OVER.instance() 
-	$".".add_child(overworld, true)	
+	call_deferred("add_child", overworld)
+	call_deferred("move_child",overworld,0)
 	if(first_over):
 		first_over=false
-		Global.pos_over=$TileMap/Position2D.get_global_position()
+		Global.pos_over = overworld.get_node("Position2D").position
 	$Player.set_global_position(Global.pos_over)
 	#camera limits
-	var map_limits = $TileMap.get_used_rect()
-	var map_cellsize = $TileMap.cell_size
+	var map_limits = overworld.get_used_rect()
+	var map_cellsize = overworld.cell_size
 	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
 	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
 	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-	$".".move_child($Player, 2)
+	currentSpace = overworld
 
 func activarJoia():
-	$TileMap.get_node("detalls").get_node("ObjecteBase").activar()
+	if currentSpace in "dung":
+		currentSpace.get_node("detalls").get_node("ObjecteBase").activar()
 
 
 func _on_TileMap_cartell():
